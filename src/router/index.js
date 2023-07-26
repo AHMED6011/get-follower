6fd2ded3e4396c7d9f1154f2c86b8068f3c796af
Your Name <you@example.com>
2023-07-26 16:55:57 +0300

    fixed bugs

--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,80 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        title: 'Ahmed_A_A_A'
+      }
     },
     {
       path: '/signup',
       name: 'signup',
-      component: () => import('../views/SignUp.vue')
+      component: () => import('../views/SignUp.vue'),
+      meta: {
+        title: 'Sign Up'
+      }
     },
     {
       path: '/faq',
       name: 'faq',
-      component: () => import('../views/FAQView.vue')
+      component: () => import('../views/FAQView.vue'),
+      meta: {
+        title: 'FAQ'
+      }
     },
     {
       path: '/service',
       name: 'service',
-      component: () => import('../views/ServicesView.vue')
+      component: () => import('../views/ServicesView.vue'),
+      meta: {
+        title: 'Services'
+      }
     },
     {
       path: '/howitworks',
       name: 'howitworks',
-      component: () => import('../views/HowItWorks.vue')
+      component: () => import('../views/HowItWorks.vue'),
+      meta: {
+        title: 'How It Works'
+      }
     },
     {
       path: '/terms',
       name: 'terms',
-      component: () => import('../views/TermsView.vue')
+      component: () => import('../views/TermsView.vue'),
+      meta: {
+        title: 'Terms'
+      }
     },
     {
       path: '/blog',
       name: 'blog',
-      component: () => import('../views/BlogsView.vue')
+      component: () => import('../views/BlogsView.vue'),
+      meta: {
+        title: 'Blogs'
+      }
     },
     {
       path: '/blog/:id',
       name: 'blogdetails',
       component: () => import('../views/BlogDetails.vue'),
+      meta: {
+        title: 'Blogs Details'
+      },
       props: true
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title}`
+  next()
 })
 
 export default router
